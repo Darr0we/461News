38f38c7f1f56c91c0d612c763c7017f83e92f35a
Jared Anouraj Chandavong <jachandavong@gmail.com>
2024-12-07 19:20:04 -0500

    Updated the About Us page

--- src/pages/AboutUsPage/AboutUs.jsx
+++ src/pages/AboutUsPage/AboutUs.jsx
@@ -3,15 +3,27 @@ import React from 'react';
 function AboutUs(){
     return (
         <div>
-          <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>
-            Hello
-          </div>
+          <br />
+          <div>
+          <h1>About Us</h1>
+          At News Feed, our mission is to empower individuals with a personalized news experience 
+          that is both relevant and engaging. We curate content based on your unique interests, preferences, 
+          and reading habits, delivering news that matters to you. Our goal is to create an informed, 
+          connected community by providing diverse, timely, and trustworthy information that helps users 
+          stay ahead of the curve, while offering tools for deeper engagement and discovery. We believe 
+          that every person deserves a news feed that speaks directly to their world.
+          </div><br />
           <h1>Members of the Team</h1>
-          <p>Trent Ross</p>
-          <p>Duc Dang</p>
-          <p>Hieu Dang</p>
-          <p>Braeden MacMaster</p>
-          <p>Jared Chandavong</p>
+          <p>Trent Ross (tnr39@drexel.edu)<br />
+          Main Developer of the MySql Database</p>
+          <p>Duc Dang (dhd37@drexel.edu)<br />
+          Main Back-End Programmer</p>
+          <p>Hieu Dang (hmd56@drexel.edu)<br />
+          Main Front-End Programmer</p>
+          <p>Braeden MacMaster (bwm35@drexel.edu)<br />
+          Data Collector & Support Specialist</p>
+          <p>Jared Chandavong (jac696@drexel.edu)<br />
+          Guy who did tests and stuff</p>
         </div>
       );
 }
