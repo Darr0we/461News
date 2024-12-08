21898eecbc391cc97b425abf95f761ac70611c8e
Trent Russ <russtr002@gmail.com>
2024-12-08 13:27:53 -0500

    Fixed topic ID display for article cards

--- src/components/card/ArticleCard.jsx
+++ src/components/card/ArticleCard.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './ArticleCard.css';
 
 function ArticleCard({ post }) {
+    const getID = () => {
+        return post.article_id
+    }
+
     return (
         <div className="card">
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}...</p> 
-            <p><strong>Topic ID:</strong> {post.topic_id}</p>
+            <p><strong>Topic ID:</strong> {getID()}</p>
             <a href={`/article/${post.article_id}`} className="btn">
                 Read More
             </a>
